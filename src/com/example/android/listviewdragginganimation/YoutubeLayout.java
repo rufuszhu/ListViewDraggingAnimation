7e4d2cad263b42e878a0b4074437444cdc8049d4
Rufus Zhu <Rufus.Zhu@digital-dispatch.com>
2014-11-05 18:03:27 -0800

    trying to solve the conflict

--- src/com/example/android/listviewdragginganimation/YoutubeLayout.java
+++ src/com/example/android/listviewdragginganimation/YoutubeLayout.java
@@ -1,11 +1,13 @@
 package com.example.android.listviewdragginganimation;
 
 import android.content.Context;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -30,6 +32,43 @@ public class YoutubeLayout extends ViewGroup {
 	private int mLeft;
 	private float mDragOffset;
 	private Context context;
+	private boolean isLongPressing = false;
+	private boolean isOnSroll = false;
+	private boolean mIsScrolling = false;
+	private int mTouchSlop;
+
+	private int mDraggingState = 0;
+	private GestureDetectorCompat mDetector;
+
+	class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+		private static final String DEBUG_TAG = "Gestures";
+
+		@Override
+		public boolean onDown(MotionEvent event) {
+			Log.d(DEBUG_TAG, "onDown: " + event.toString());
+			return true;
+		}
+
+		@Override
+		public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+			Log.d(DEBUG_TAG, "onFling: " + event1.toString() + event2.toString());
+			return true;
+		}
+
+		@Override
+		public void onLongPress(MotionEvent event) {
+			Log.d(DEBUG_TAG, "onLongPress: " + event.toString());
+			isLongPressing = true;
+		}
+
+		@Override
+		public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+			Log.d(DEBUG_TAG, "onScroll: " + e1.toString() + e2.toString());
+			isOnSroll = true;
+			return true;
+		}
+
+	}
 
 	public YoutubeLayout(Context context) {
 		this(context, null);
@@ -39,6 +78,7 @@ public class YoutubeLayout extends ViewGroup {
 	public YoutubeLayout(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 		this.context = context;
+		mDetector = new GestureDetectorCompat(context, new MyGestureListener());
 	}
 
 	@Override
@@ -67,25 +107,34 @@ public class YoutubeLayout extends ViewGroup {
 		mDragHelper = ViewDragHelper.create(this, 1f, new DragHelperCallback());
 	}
 
-	public void maximize() {
-		smoothSlideTo(mDescView.getMeasuredWidth());
-	}
-
-	public void minimize() {
-		smoothSlideTo(0);
-	}
-
-	boolean smoothSlideTo(float slideOffset) {
-		final int leftBound = getPaddingLeft();
-		int x = (int) (leftBound + slideOffset);
-		if (mDragHelper.smoothSlideViewTo(mHeaderView, x, mHeaderView.getTop())) {
-			ViewCompat.postInvalidateOnAnimation(this);
-			return true;
-		}
-		return false;
-	}
+	// public void maximize() {
+	// smoothSlideTo(mDescView.getMeasuredWidth());
+	// }
+	//
+	// public void minimize() {
+	// smoothSlideTo(0);
+	// }
+	//
+	// boolean smoothSlideTo(float slideOffset) {
+	// final int leftBound = getPaddingLeft();
+	// int x = (int) (leftBound + slideOffset);
+	// if (mDragHelper.smoothSlideViewTo(mHeaderView, x, mHeaderView.getTop())) {
+	// ViewCompat.postInvalidateOnAnimation(this);
+	// return true;
+	// }
+	// return false;
+	// }
 
 	private class DragHelperCallback extends ViewDragHelper.Callback {
+
+		@Override
+		public void onViewDragStateChanged(int state) {
+			if (state == mDraggingState) { // no change
+				return;
+			}
+			Log.e(TAG, "onViewDragStateChanged");
+			mDraggingState = state;
+		}
 
 		@Override
 		public boolean tryCaptureView(View child, int pointerId) {
@@ -94,38 +143,20 @@ public class YoutubeLayout extends ViewGroup {
 
 		@Override
 		public void onViewPositionChanged(View changedView, int left, int top, int dx, int dy) {
-			// Log.e(TAG,"onViewPositionChanged");
-			// mTop = top;
-			// mLeft = mHeaderView.getLeft();
+
 			mLeft = left;
-			// Log.e(TAG,"onViewPositionChanged mHeaderView.getLeft():" + mHeaderView.getLeft());
-			// Log.e(TAG,"************************************");
-			// mDragOffset = (float) left / mDragRange;
-			//
-			// mHeaderView.setPivotX(mHeaderView.getWidth());
-			// mHeaderView.setPivotY(mHeaderView.getHeight());
-			// mHeaderView.setScaleX(1 - mDragOffset / 2);
-			// mHeaderView.setScaleY(1 - mDragOffset / 2);
-			//
-			// mDescView.setAlpha(1 - mDragOffset);
 
 			requestLayout();
 		}
 
 		@Override
 		public void onViewReleased(View releasedChild, float xvel, float yvel) {
-			// Log.e(TAG,"xvel: " + xvel);
-			// Log.e(TAG,"percent: " + (float) mLeft/mDescView.getWidth());
-			// Log.e(TAG,"onViewReleased");
 			if (xvel > 0 || (xvel == 0 && (float) mLeft / mDescView.getWidth() > 0.5f)) {
 				mDragHelper.settleCapturedViewAt(mDescView.getMeasuredWidth(), releasedChild.getTop());
-				// maximize();
 			} else {
-				// minimize();
 				mDragHelper.settleCapturedViewAt(0, releasedChild.getTop());
 			}
 			invalidate();
-
 		}
 
 		@Override
@@ -155,99 +186,124 @@ public class YoutubeLayout extends ViewGroup {
 
 	@Override
 	public boolean onInterceptTouchEvent(MotionEvent ev) {
-		final int action = MotionEventCompat.getActionMasked(ev);
-
-		if ((action != MotionEvent.ACTION_DOWN)) {
-			Log.e(TAG,"onInterceptTouchEvent ACTION_DOWN cancel");
-			mDragHelper.cancel();
-			return super.onInterceptTouchEvent(ev);
-		}
-
-		if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
-			Log.e(TAG,"onInterceptTouchEvent ACTION_CANCEL||ACTION_UP");
-			mDragHelper.cancel();
-			return false;
-		}
 
 		final float x = ev.getX();
 		final float y = ev.getY();
-		boolean interceptTap = false;
+
+		boolean result = false;
+		final int action = MotionEventCompat.getActionMasked(ev);
+		// Always handle the case of the touch gesture being complete.
+		if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+			// Release the scroll.
+			mIsScrolling = false;
+			return false; // Do not intercept touch event, let the child handle it
+		}
 
 		switch (action) {
 		case MotionEvent.ACTION_DOWN: {
 			Log.e(TAG, "onInterceptTouchEvent ACTION_DOWN");
 			mInitialMotionX = x;
 			mInitialMotionY = y;
-			// interceptTap = mDragHelper.isViewUnder(mHeaderView, (int) x, (int) y);
 			break;
 		}
 
 		case MotionEvent.ACTION_MOVE: {
 			Log.e(TAG, "onInterceptTouchEvent ACTION_MOVE");
-			final float adx = Math.abs(x - mInitialMotionX);
-			final float ady = Math.abs(y - mInitialMotionY);
-			final int slop = mDragHelper.getTouchSlop();
-			Log.e(TAG, "onInterceptTouchEvent slop: " + slop);
-			// if (ady > slop && adx > ady) {
-			// mDragHelper.cancel();
-			// return false;
-			// }
-			if (adx > 0) {
+			if (mIsScrolling) {
+				// We're currently scrolling, so yes, intercept the
+				// touch event!
 				return true;
 			}
-		}
+
+			// If the user has dragged her finger horizontally more than
+			// the touch slop, start the scroll
+
+			// left as an exercise for the reader
+			final int xDiff = (int) x - (int) mInitialMotionX;
+
+			// Touch slop should be calculated using ViewConfiguration
+			// constants.
+			if (xDiff > mDragHelper.getTouchSlop()) {
+				// Start scrolling!
+				mIsScrolling = true;
+				return true;
+			}
+			break;
 		}
 
-	 //return mDragHelper.shouldInterceptTouchEvent(ev) || interceptTap;
+		}
+
+		// In general, we don't want to intercept touch events. They should be
+		// handled by the child view.
 		return false;
+
+		// if (mDragHelper.shouldInterceptTouchEvent(event)) {
+		// Log.e(TAG, "onInterceptTouchEvent true");
+		// return true;
+		// } else {
+		// Log.e(TAG, "onInterceptTouchEvent false");
+		// return false;
+		// }
 	}
 
-	// @Override
-	public boolean onTouchEvent(MotionEvent ev) {
-		mDragHelper.processTouchEvent(ev);
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		// this.mDetector.onTouchEvent(event);
+		// if (event.getAction() == MotionEvent.ACTION_UP) {
+		// isLongPressing = false;
+		// isOnSroll = false;
+		// }
+		//
+		// if (isLongPressing)
+		// return super.onTouchEvent(event);
+		// if (isOnSroll) {
+		// if (mDragHelper != null)
+		// Log.e(TAG, "mDragHelper is not null");
+		//
+		// if (event != null)
+		// Log.e(TAG, "event is not null");
+		//
+		// mDragHelper.processTouchEvent(event);
+		// return true;
+		// }
+		//
+		// return true;
+		final float x = event.getX();
+		final float y = event.getY();
 
-		final int action = ev.getAction();
-		final float x = ev.getX();
-		final float y = ev.getY();
-
-		boolean isHeaderViewUnder = mDragHelper.isViewUnder(mHeaderView, (int) x, (int) y);
-		switch (action & MotionEventCompat.ACTION_MASK) {
+		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN: {
 			Log.e(TAG, "onTouchEvent ACTION_DOWN");
 			mInitialMotionX = x;
 			mInitialMotionY = y;
 			break;
 		}
-
 		case MotionEvent.ACTION_MOVE: {
+			Log.e(TAG, "onTouchEvent ACTION_MOVE");
+			final float adx = Math.abs(x - mInitialMotionX);
+			final float ady = Math.abs(y - mInitialMotionY);
 
-			final float dx = x - mInitialMotionX;
-			Log.e(TAG, "on touch on move, dx: " + dx);
-			if (dx > 0) {
+			if (adx > mDragHelper.getTouchSlop()) {
+				mDragHelper.processTouchEvent(event);
 				return true;
 			}
-
-			break;
-		}
-
-		case MotionEvent.ACTION_UP: {
-			Log.e(TAG, "onTouchEvent ACTION_UP");
-
-			final int slop = mDragHelper.getTouchSlop();
-			Log.e(TAG, "slop: " + slop);
-			// if (dx * dx < slop * slop && isHeaderViewUnder) {
-			// if (mDragOffset == 0) {
-			// smoothSlideTo(0);
-			// } else {
-			// maximize();
-			// }
-			// }
-			break;
 		}
 		}
+		
+		return super.onTouchEvent(event);
 
-	 return isHeaderViewUnder && isViewHit(mHeaderView, (int) x, (int) y) || isViewHit(mDescView, (int) x, (int) y);
-		//return super.onTouchEvent(ev);
+		// if (result && isMoving()) {
+		// Log.e(TAG, "onTouchEvent returning true");
+		// mDragHelper.processTouchEvent(event);
+		// return true;
+		// } else {
+		// Log.e(TAG, "onTouchEvent returning false");
+		// return super.onTouchEvent(event);
+		// }
+	}
+
+	public boolean isMoving() {
+		return (mDraggingState == ViewDragHelper.STATE_DRAGGING || mDraggingState == ViewDragHelper.STATE_SETTLING);
 	}
 
 	private boolean isViewHit(View view, int x, int y) {
